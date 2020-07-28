68b5cdb7c4c66c679ca2fbee57afd40bc2aac76b
samh2575 <samharrison2575@gmail.com>
2020-07-28 16:36:42 +0200

    markdown

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -17,5 +16,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def body_summary(self):
+    def text_summary(self):
         return (self.text[:300] + "...")
